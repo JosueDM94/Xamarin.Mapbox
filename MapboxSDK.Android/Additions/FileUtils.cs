5d34933f4dafcb813da606faf04b2928572900fc
Josué Emmanuel Díaz Mayorga <jdiaz@itexico.net>
2019-02-20 09:34:11 -0600

    -Initial Binding project

---
+++ MapboxSDK.Android/Additions/FileUtils.cs
@@ -0,0 +1,25 @@
+﻿using Android.Runtime;
+
+namespace Mapbox.MapboxSDK.Utils
+{
+    public partial class FileUtils
+    {
+        // Error CS0534: 'FileUtils.CheckFileReadPermissionTask' does not implement inherited abstract member 'AsyncTask.DoInBackground(params Object[])'
+        public partial class CheckFileReadPermissionTask : global::Android.OS.AsyncTask
+        {
+            protected override Java.Lang.Object DoInBackground(params Java.Lang.Object[] @params)
+            {
+                return new Java.Lang.Object(JNIEnv.CallObjectMethod(Handle, JNIEnv.GetMethodID(JNIEnv.GetObjectClass(Handle), "doInBackground", "([Ljava/io/File;)Ljava/lang/Boolean;")), JniHandleOwnership.TransferLocalRef);
+            }
+        }
+
+        // Error CS0534: 'FileUtils.CheckFileWritePermissionTask' does not implement inherited abstract member 'AsyncTask.DoInBackground(params Object[])'
+        public partial class CheckFileWritePermissionTask : global::Android.OS.AsyncTask
+        {
+            protected override Java.Lang.Object DoInBackground(params Java.Lang.Object[] @params)
+            {
+                return new Java.Lang.Object(JNIEnv.CallObjectMethod(Handle, JNIEnv.GetMethodID(JNIEnv.GetObjectClass(Handle), "doInBackground", "([Ljava/io/File;)Ljava/lang/Boolean;")), JniHandleOwnership.TransferLocalRef);
+            }
+        }
+    }
+}
---
+++ MapboxSDK.Android/Additions/MarkerOptions.cs
@@ -0,0 +1,24 @@
+﻿using Android.Runtime;
+
+namespace Mapbox.Mapboxsdk.Annotations
+{
+    public partial class MarkerOptions //: global::Mapbox.Mapboxsdk.Annotations.BaseMarkerOptions, global::Android.OS.IParcelable
+    {
+        //protected override Java.Lang.Object RawMarker
+        //{
+        //    get
+        //    {
+        //        return new Java.Lang.Object(JNIEnv.CallObjectMethod(Handle, JNIEnv.GetMethodID(JNIEnv.GetObjectClass(Handle), "getMarker", "()Lcom/mapbox/mapboxsdk/annotations/Marker;")), JniHandleOwnership.TransferLocalRef);
+        //    }
+        //}
+
+        //// Error CS0534: 'MarkerOptions' does not implement inherited abstract member 'BaseMarkerOptions.RawThis.get'
+        //protected override Java.Lang.Object RawThis
+        //{
+        //    get
+        //    {
+        //        return new Java.Lang.Object(JNIEnv.CallObjectMethod(Handle, JNIEnv.GetMethodID(JNIEnv.GetObjectClass(Handle), "getThis", "()Lcom/mapbox/mapboxsdk/annotations/BaseMarkerOptions;")), JniHandleOwnership.TransferLocalRef);
+        //    }
+        //}
+    }
+}
